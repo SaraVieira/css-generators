3ef7a26b81814bbcc9fd36d2967487de441f993e
Sara Vieira <hey@iamsaravieira.com>
2023-02-26 22:59:18 +0100

    add some SEO

---
+++ components/SEO.vue
@@ -0,0 +1,58 @@
+<template>
+    <Head>
+      <!-- Primary Meta Tags -->
+      <title>CSS Generators</title>
+      <meta name="title" content="CSS Generators" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta
+        name="description"
+        content="Create stunning CSS effects with ease using our collection of free online generators. Generate custom box shadows, gradients, borders, and more for your website design projects."
+      />
+
+      <!-- Open Graph / Facebook -->
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://css-generators.dotenv.dev/" />
+      <meta property="og:title" content="CSS Generators" />
+      <meta
+        property="og:description"
+        content="Create stunning CSS effects with ease using our collection of free online generators. Generate custom box shadows, gradients, borders, and more for your website design projects."
+      />
+      <meta
+        property="og:image"
+        content="https://css-generators.dotenv.dev/meta.png"
+      />
+
+      <!-- Twitter -->
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://css-generators.dotenv.dev/"
+      />
+      <meta property="twitter:title" content="CSS Generators" />
+      <meta
+        property="twitter:description"
+        content="Create stunning CSS effects with ease using our collection of free online generators. Generate custom box shadows, gradients, borders, and more for your website design projects."
+      />
+      <meta
+        property="twitter:image"
+        content="https://css-generators.dotenv.dev/meta.png"
+      />
+    </Head>
+</template>
